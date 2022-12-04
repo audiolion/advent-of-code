23f87d4b7e9c21bb547c75d553ebebc8c6283b54
Ryan Castner <castner.rr@gmail.com>
2022-12-04 14:02:48 -0500

    2022 Day 4 tidy up

--- 2022/04/main.py
+++ 2022/04/main.py
@@ -3,22 +3,16 @@ def run():
         input = f.read()
 
     subset_count = 0
-    for line in input.split("\n"):
-        section_1, section_2 = [list(map(int, x.split('-'))) for x in line.split(",")]
-        if section_1[0] >= section_2[0] and section_1[1] <= section_2[1]:
-            subset_count += 1
-        elif section_1[0] <= section_2[0] and section_1[1] >= section_2[1]:
-            subset_count += 1
-
-    print(subset_count)
-
-
     overlap_count = 0
     for line in input.split("\n"):
         section_1, section_2 = [list(map(int, x.split('-'))) for x in line.split(",")]
         range_1, range_2 = set(range(section_1[0], section_1[1]+1)), set(range(section_2[0], section_2[1]+1))
         if not range_1.isdisjoint(range_2):
             overlap_count += 1
+        if range_1.issubset(range_2) or range_1.issuperset(range_2):
+            subset_count += 1
+
+    print(subset_count)
 
     print(overlap_count)
 
