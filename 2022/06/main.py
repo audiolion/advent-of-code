16cc57426e64439041d884d1e0adc8df6b325b3f
Ryan Castner <castner.rr@gmail.com>
2022-12-06 06:51:23 -0500

    2022: Day 6 Python + tidy

--- 2022/06/main.py
+++ 2022/06/main.py
@@ -2,25 +2,20 @@ def run():
     with open(0) as f:
         input = f.read()
 
-    for line in input.split("\n"):
-        start_of_packet_idx = 0
-        j = 4
-        for i in range(0, len(line)):
-            if len(set(line[i:j])) == 4:
-                start_of_packet_idx = j
-                break
+    def find_unique_window_idx(input, size):
+        j = size
+        for i in range(0, len(input)):
+            if len(set(input[i:j])) == size:
+                return j
             j += 1
+        return -1
 
+
+    for line in input.split("\n"):
+        start_of_packet_idx = find_unique_window_idx(line, 4)
         print(start_of_packet_idx)
 
-        j = 14
-        start_of_message_idx = 0
-        for i in range(0, len(line)):
-            if len(set(line[i:j])) == 14:
-                start_of_message_idx = j
-                break
-            j += 1
-
+        start_of_message_idx = find_unique_window_idx(line, 14)
         print(start_of_message_idx)
 
 
